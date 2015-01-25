5189cbb2afc5ea01cdd733199db4e6438ad89287
Mattias Lundell <mattias@lundell.com>
2015-01-25 01:58:39 +0100

    proper animation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate sdl2_window;
 extern crate opengl_graphics;
 extern crate shader_version;
 extern crate event;
+extern crate quack;
 
 use sdl2_window::Sdl2Window as Window;
 use opengl_graphics::Gl;
@@ -13,7 +14,7 @@ use std::collections::HashSet;
 use std::cell::RefCell;
 use piston::{
     RenderArgs,
-    UpdateArgs
+    UpdateArgs,
 };
 
 use graphics::*;
@@ -21,7 +22,14 @@ use graphics::*;
 use event::{
     RenderEvent,
     UpdateEvent,
+    WindowSettings,
+    Events,
+    Event,
+    Ups,
+    MaxFps,
 };
+use quack::Set;
+
 
 pub static WINDOW_HEIGHT: i32 = 600;
 pub static WINDOW_WIDTH: i32 = 600;
@@ -30,7 +38,7 @@ pub static BLOCKSIZE: f64 = 10.0;
 pub static BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 pub static WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
-#[derive(PartialEq, Eq, Hash, Clone, Show)]
+#[derive(PartialEq, Eq, Hash, Clone, Show, Copy)]
 pub struct Cell { x: i32, y: i32 }
 impl Cell {
     pub fn new(x: i32, y: i32) -> Cell {
@@ -52,6 +60,15 @@ pub struct World {
 impl World {
     pub fn new() -> World {
         let mut grid: Grid = HashSet::new();
+
+        let g = Cell::new(2, 1);
+        let h = Cell::new(2, 2);
+        let i = Cell::new(2, 3);
+
+        grid.insert(g);
+        grid.insert(h);
+        grid.insert(i);
+
         World {grid: grid}
     }
 
@@ -63,18 +80,18 @@ impl World {
 
     fn step(&mut self) -> () {
         let mut grid: Grid = HashSet::new();
-        let neighbors =  self.neighbors(&self.grid);
+        let neighbors =  self.neighbors();
         for cell in neighbors.iter() {
+            let n_alive: i32 = self.count_neighbors(cell);
             // alive
             if self.grid.contains(cell) {
-                let n_alive: i32 = self.count_neighbors(&self.grid, cell);
                 if n_alive == 2 || n_alive == 3 {
                     grid.insert(cell.clone());
+                } else {
                 }
             }
             // dead
             else {
-                let n_alive: i32 = self.count_neighbors(&self.grid, cell);
                 if n_alive == 3 {
                     grid.insert(cell.clone());
                 }
@@ -85,7 +102,7 @@ impl World {
     }
 
 
-    fn neighbors(&self, grid: &Grid) -> Grid {
+    fn neighbors(&self) -> Grid {
         let mut neighbors: Grid = HashSet::new();
         for cell in self.grid.iter() {
             for &i in [-1, 0, 1].iter() {
@@ -97,12 +114,12 @@ impl World {
         neighbors
     }
 
-    fn count_neighbors(&self, grid: &Grid, cell: &Cell) -> i32 {
+    fn count_neighbors(&self, cell: &Cell) -> i32 {
         let mut n = 0;
         for &i in [-1, 0, 1].iter() {
             for &j in [-1, 0, 1].iter() {
-                if i != 0 && j != 0 {
-                    if grid.contains(&Cell::new(cell.x + i, cell.y + j)) {
+                if !(i == 0 && j == 0) {
+                    if self.grid.contains(&Cell::new(cell.x + i, cell.y + j)) {
                         n += 1;
                     }
                 }
@@ -117,8 +134,11 @@ pub struct App {
     gl: Gl,
     world: World
 }
-
 impl App {
+    fn new() -> App {
+        App { gl: Gl::new(_3_2), world: World::new() }
+    }
+
     fn render(&mut self, _: &mut Window, args: &RenderArgs) {
         let w = args.width as f64;
         let h = args.height as f64;
@@ -126,20 +146,11 @@ impl App {
         let ctx = Context::abs(w, h);
         graphics::clear(WHITE, &mut self.gl);
 
-        let f = Cell::new(0, 0);
-        let g = Cell::new(1, 1);
-        let h = Cell::new(2, 2);
-        let i = Cell::new(3, 3);
-
-        self.world.grid.insert(f);
-        self.world.grid.insert(g);
-        self.world.grid.insert(h);
-        self.world.grid.insert(i);
-        self.world.step();
         self.world.render(&ctx, &mut self.gl);
     }
 
-    fn update(&mut self, _: &mut Window, args: &UpdateArgs) {
+    fn update(&mut self, _: &mut Window) {
+        self.world.step();
     }
 }
 
@@ -148,15 +159,16 @@ fn main() {
         _3_2,
         piston::WindowSettings::default());
 
-    let mut app = App { gl: Gl::new(_3_2), world: World::new() };
+    let mut app = App::new();
 
     let window = RefCell::new(window);
-    for e in event::events(&window) {
-        if let Some(r) = e.render_args() {
-            app.render(&mut *window.borrow_mut(), &r);
-        }
-        if let Some(u) = e.update_args() {
-            app.update(&mut *window.borrow_mut(), &u);
-        }
+
+    for e in event::events(&window).set(Ups(2)).set(MaxFps(60)) {
+        e.render(|args| {
+            app.render(&mut *window.borrow_mut(), args);
+        });
+        e.update(|_| {
+            app.update(&mut *window.borrow_mut());
+        });
     }
 }
